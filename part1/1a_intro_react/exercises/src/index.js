239a1dd718d13d41d8aa8754c7de17ce00020c3b
dancar528 <dancar.528@gmail.com>
2019-09-03 08:30:29 -0500

    Preparing Step 1

--- part1/1a_intro_react/exercises/src/index.js
+++ part1/1a_intro_react/exercises/src/index.js
@@ -1,84 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
-// import * as serviceWorker from './serviceWorker';
-const Hello = (props) => {
-    return (
-        <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
-        </div>
-    );
-};
-
-/*const App = () => {
-    const name = "Peter";
-    const age = 10;
-    return [
-        
-            <p>Greetings</p>,
-            <Hello name="George" age={26 + 10}/>,
-            <Hello name={name} age={age}/>
-        
-    ];
-};*/
+import ReacDOM from 'react-dom';
 
 const App = () => {
-    const name = "Peter";
-    const age = 10;
-    return (
-        <>
-            <p>Greetings</p>
-            <Hello name="George" age={26 + 10}/>
-            <Hello name={name} age={age}/>
-        </>
-    );
+  const course = 'Half Stack application Development';
+  const part1 = 'Fundamentals of react';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <h1>{course}</h1>
+        <p>
+          {part1} {exercises1}
+        </p>
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+        <p>Number of execises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-/*
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const App = () => {
-    const now = new Date();
-    const a = 10;
-    const b = 20;
-
-    return (
-        <div>
-            <p>Hello world, it is {now.toString()}</p>
-            <p>{a} + {b} is {a + b}</p>
-        </div>
-    );
-};
-
-would compile as (by babel):
-
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
-  )
-}
-
-ReactDOM.render(
-  React.createElement(App, null),
-  document.getElementById('root')
-)
-*/
+ReacDOM.render(<App />, document.getElementById('root'));
