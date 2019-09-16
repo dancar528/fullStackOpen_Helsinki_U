94c64ba3a9987dc14e7eb981f5b988d301cf43fc
dancar528 <dancar.528@gmail.com>
2019-09-15 23:07:23 -0500

    1.12 anecdotes Step 1

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const [anecdote, setAnecdote] = useState(0);
+    const anecdotes = [
+        'Brooks Law: "Adding manpower to a late software project makes it later!"',
+        `The best way to get a project done faster is to start sooner 
+        --Jim Highsmith`,
+        `The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.
+        -- Tom Cargill`,
+        `Even the best planning is not so omniscient as to get it right the first time.
+        -- Fred Brooks`,
+        `How does a project get to be a year late?... One day at a time.
+        -- Fred Brooks`,
+        `Plan to throw one (implementation) away; you will, anyhow.
+        -- Fred Brooks`,
+        `Every good work of software starts by scratching a developer's personal itch`,
+        `Perfection (in design) is achieved not when there is nothing more to add, but rather when there is nothing more to take away
+        --Antoine de Saint-Exupery`,
+        `Any fool can write code that a computer can understand. Good programmers write code that humans can understand.
+        --Martin Fowler`,
+        `Program testing can be used to show the presence of bugs, but never to show their absence!
+        --Edsger Dijkstra`,
+        `A primary cause of complexity is that software vendors uncritically adopt almost any feature that users want.
+        --Niklaus Wirth`
+    ];
+
+    const getAnecdote = () => {
+        const randomIndex = Math.floor(Math.random() * anecdotes.length);
+        setAnecdote(randomIndex);
+    };
+
+    return (
+        <div>
+            <p>{anecdotes[anecdote]}</p>
+            <button onClick={getAnecdote}>Next anecdote</button>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
