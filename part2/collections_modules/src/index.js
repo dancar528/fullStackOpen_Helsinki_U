150f92694e65a49fae4f5ddf218a727d74e93b6b
dancar528 <dancar.528@gmail.com>
2019-09-17 23:16:30 -0500

    2.4 Course contents step 9

--- part2/collections_modules/src/index.js
+++ part2/collections_modules/src/index.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const course = {
-    name: 'Half Stack application development',
-    parts: [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-        },
-        {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-        }
-    ]
-};
+const courses = [
+    {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 4
+            }
+        ]
+    },
+    {
+        name: "Node.js",
+        id: 2,
+        parts: [
+            {
+                name: 'Routing',
+                exercises: 3,
+                id: 1
+            },
+            {
+                name: 'Middlewares',
+                exercises: 7,
+                id: 2
+            }
+        ]
+    }
+];
 
 const Total = ({ parts }) => {
     const total = parts.reduce(
@@ -53,11 +77,11 @@ const Course = ({ name, parts }) => (
     </div>
 );
 
-const App = ({ course }) => (
-    <Course name={course.name} parts={course.parts} />
+const App = ({ courses }) => courses.map(course =>
+    <Course key={course.id} name={course.name} parts={course.parts} />
 );
 
 ReactDOM.render(
-    <App course={course} />,
+    <App courses={courses} />,
     document.getElementById('root')
 );
