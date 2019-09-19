42f738073f8bcc6f9b40203870672dcc9d116ccf
dancar528 <dancar.528@gmail.com>
2019-09-18 23:22:10 -0500

    2.9* the phonebook step 4

--- part2/forms/src/components/App.js
+++ part2/forms/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '23-234-2342' }
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ]);
-    const [ newName, setNewName ] = useState('');
-    const [ newNumber, setNewNumber ] = useState('');
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [newSearch, setNewSearch] = useState('');
 
     const handleNameChange = (event) => {
         setNewName(event.target.value);
@@ -13,6 +17,10 @@ const App = () => {
 
     const handleNumberChange = (event) => {
         setNewNumber(event.target.value);
+    };
+
+    const handleSearchChange = (event) => {
+        setNewSearch(event.target.value);
     };
 
     const handleFormSubmit = (event) => {
@@ -32,13 +40,23 @@ const App = () => {
         setNewNumber('');
     };
 
-    const rows = () => persons.map((person, index) =>
-        <div key={index}>{person.name} {person.number}</div>
-    );
+    const rows = () => {
+        const personsFilter = newSearch
+            ? persons.filter(person =>
+                person.name.toLowerCase().indexOf(newSearch.toLowerCase()) !== -1
+            )
+            : persons;
+        return personsFilter.map((person, index) =>
+            <div key={index}>{person.name} {person.number}</div>
+        );
+    };
 
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with <input value={newSearch} onChange={handleSearchChange} />
+            </div>
             <form onSubmit={handleFormSubmit}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
